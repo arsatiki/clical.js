87defa35ae0f38191684f9f4697bb0a2a11d03fd
Antti Rasinen <antti.rasinen@codento.com>
2012-02-23 23:16:35 +0200

    Port parity checking over to JS

--- js/permutations.js
+++ js/permutations.js
@@ -1,5 +1,35 @@
+function cmp(a, b) { return a - b; }
+
+function same_parity(items, target) {
+	var k;
+	var f = {};
+	var seen = {};
+	var even_cycles = 0;
+
+	for (k = 0; k < items.length; k++)
+		f[target[k]] = items[k];
+
+	for (k = 0; k < items.length; k++) {
+		if (items[k] in seen)
+			continue;
+
+		var c = items[k];
+		var cycle_length = 0;
+		while (!(c in seen)) {
+			seen[c] = true;
+			cycle_length++;
+			c = f[c];
+		}
+
+		even_cycles += (cycle_length - 1) % 2;
+	}
+
+	return (even_cycles % 2 == 0)? 1: -1;
+}
+
 function parity(items) {
-	return 1; // Just for testing purposes at the moment.
+	var sorted = items.slice(0).sort(cmp);
+	return same_parity(items, sorted);
 }
 
 exports.parity = parity;
--- test/test_permutations.js
+++ test/test_permutations.js
@@ -3,6 +3,6 @@ var clical = require('clical'),
 
 exports.testParity = function(test) {
 	assert.equal(clical.parity([1, 2, 3]), 1);
-	//assert.equal(clical.parity([2, 1, 3]), -1);
+	assert.equal(clical.parity([2, 1, 3]), -1);
 	test.done();
 }
