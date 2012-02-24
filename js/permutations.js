8c3e5e1aacf340033b67cdb3114111ffe8780f76
Antti Rasinen <antti.rasinen@codento.com>
2012-02-24 13:34:15 +0200

    Replace permutation-based sign determination with selection sort
    - The idea is from Joel Kaasinen

--- js/permutations.js
+++ js/permutations.js
@@ -1,35 +1,21 @@
-function cmp(a, b) { return a - b; }
+function swaps(items) {
+	var i, j, minj;
+	var cswaps = 0, smallest;
+	
+	for (i = 0; i < items.length; i++) {
+		minj = i;
+		for (j = i + 1; j < items.length; j++)
+			if (items[j] < items[minj])
+				minj = j;
 
-function same_parity(items, target) {
-	var k;
-	var f = {};
-	var seen = {};
-	var even_cycles = 0;
-
-	for (k = 0; k < items.length; k++)
-		f[target[k]] = items[k];
-
-	for (k = 0; k < items.length; k++) {
-		if (items[k] in seen)
-			continue;
-
-		var c = items[k];
-		var cycle_length = 0;
-		while (!(c in seen)) {
-			seen[c] = true;
-			cycle_length++;
-			c = f[c];
+		if (minj != i) {
+			smallest = items[minj];
+			items[minj] = items[i];
+			items[i] = smallest;
+			cswaps++;
 		}
-
-		even_cycles += (cycle_length - 1) % 2;
 	}
-
-	return (even_cycles % 2 == 0)? 1: -1;
+	return (cswaps % 2 == 0)? 1 : -1;
 }
 
-function parity(items) {
-	var sorted = items.slice(0).sort(cmp);
-	return same_parity(items, sorted);
-}
-
-exports.parity = parity;
+exports.swaps = swaps;
--- test/test_permutations.js
+++ test/test_permutations.js
@@ -1,8 +1,27 @@
-var clical = require('clical'),
+var perms = require('../js/permutations.js'),
     assert = require('assert');
 
-exports.testParity = function(test) {
-	assert.equal(clical.parity([1, 2, 3]), 1);
-	assert.equal(clical.parity([2, 1, 3]), -1);
+function assertSorted(items) {
+	var k;
+	for (k = 1; k < items.length; k++)
+		assert.ok(items[k - 1] < items[k]);
+}
+
+exports.testSwapPos = function(test) {
+	var items = [1, 2, 3, 4, 5];
+	var sign = perms.swaps(items);
+	assert.equal(sign, 1)
+	assertSorted(items);
+
 	test.done();
 }
+
+exports.testSwapNeg = function(test) {
+	var items = [1, 2, 5, 4, 3];
+	var sign = perms.swaps(items);
+	assert.equal(sign, -1)
+	assertSorted(items);
+
+	test.done();
+}
+
