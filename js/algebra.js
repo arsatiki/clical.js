d86909a85f5b8aa2beb0e217678aba7507d749b2
Antti Rasinen <antti.rasinen@codento.com>
2012-02-26 13:26:54 +0200

    Add stubs for Term and Multivector

---
+++ js/algebra.js
@@ -0,0 +1,19 @@
+//var exports = exports || {};
+//var require = require || function() {};
+//var permutations = require("./permutations.js");
+
+var Term = function (coefficient, dimensions) {
+	this.coefficient = coefficient;
+	this.dimensions = dimensions.slice(0);
+	
+	var n = swaps(this.dimensions);
+	this.coefficient *= n;
+}
+
+var Multivector = function () {
+	this.terms = Array.prototype.slice.call(arguments);
+	// TODO: Cleanup
+}
+
+
+//exports.multivector = multivector;
--- js/permutations.js
+++ js/permutations.js
@@ -1,7 +1,9 @@
+var exports = exports || {};
+
 function swaps(items) {
 	var i, j, minj;
 	var cswaps = 0, smallest;
-	
+
 	for (i = 0; i < items.length; i++) {
 		minj = i;
 		for (j = i + 1; j < items.length; j++)
@@ -17,5 +19,6 @@ function swaps(items) {
 	}
 	return (cswaps % 2 == 0)? 1 : -1;
 }
+
 
 exports.swaps = swaps;
