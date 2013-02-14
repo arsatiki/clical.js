e90ca1d32a7ec38a275a3fa64d17fb401c82b1cf
Antti Rasinen <ars@iki.fi>
2013-02-14 15:23:20 +0200

    Add initial version of MathML output converter

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,48 @@
+function createMathElement(name) {
+	var MATHML = "http://www.w3.org/1998/Math/MathML";
+	return document.createElementNS(MATHML, name);
+}
+
+
+function createTermTree(parent, coeff, bases) {
+	var c, e, b, sub;
+	c = createMathElement("mn");
+	e = createMathElement("mi");
+	b = createMathElement("mn");
+	sub = createMathElement("msub");
+	
+	c.appendChild(document.createTextNode(coeff));
+	e.appendChild(document.createTextNode("e"))
+	b.appendChild(document.createTextNode(bases));
+	sub.appendChild(e);
+	sub.appendChild(document.createTextNode(" "));
+	sub.appendChild(b);
+
+	parent.appendChild(c);
+	parent.appendChild(document.createTextNode("&InvisibleTimes;"));
+	parent.appendChild(sub);
+}
+
+function toMathML(mv) {
+	var k, t;
+
+	var NS = "";
+	var math = createMathElement("math");
+	var mrow = createMathElement("mrow");
+	math.appendChild(mrow);
+	
+	for (k=0; k < mv.terms.length; k++) {
+		t = mv.terms[k];
+		createTermTree(mrow, t.coefficient, t.dimensions.join(""));
+	}
+	var li = document.createElement("li");
+	li.appendChild(math);
+	return li;
+}
+
 function lifmt(value, answer) {
-	var input = $('<span class="user_input" />').append(value);
-	var ansp = $('<span class="answer">').append("ans = " + answer);
+	var input = $('<kbd />').append(value);
+	var ansp = $('<output form="input" />').append("ans = " + answer);
 	return $("<li/>").append(input).append(ansp);
 	
 }
@@ -9,9 +51,10 @@ function eval_input(event) {
 	var value = event.target.value;
 	event.target.value = "";
 
-	var parsed = clicalparser.parse(value);
+	/*var parsed = clicalparser.parse(value);*/
 
-	$("#results").append(lifmt(value, parsed));
+	/*$("#results").append(lifmt(value, parsed));*/
+	$("#results").append(toMathML(v(1.0, 2.0, 3.0)));
 	event.stopPropagation();
 }
 
