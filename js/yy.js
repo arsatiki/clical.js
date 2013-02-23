7cbdb6976f159f185ff9fd3b6d542a8a6ad24f22
Antti Rasinen <ars@iki.fi>
2013-02-23 10:12:05 +0200

    Wrap yy object in a closure wrapper.

--- js/yy.js
+++ js/yy.js
@@ -1,21 +1,25 @@
-var yy = {
-	assignment: function (identifier, exp) {},
-	identifier: function (name) {},
+var yy = function(){
+	var yyobj = {
+		assignment: function (identifier, exp) {},
+		identifier: function (name) {},
 
-	negate: function (exp) {},
-	conjugate: function (exp) {},
-	involute: function (exp) {},
+		negate: function (exp) {},
+		conjugate: function (exp) {},
+		involute: function (exp) {},
 
-	add: function(exp1, exp2) {},
-	subtract: function(exp1, exp2) {},
-	power: function(exp1, exp2) {},
-	outerPower: function(exp1, exp2) {},
-	multiply: function(exp1, exp2) {},
-	innerProduct: function(exp1, exp2) {},
-	outerProduct: function(exp1, exp2) {},
-	div: function(exp1, exp2) {},
-	backdiv: function(exp1, exp2) {},
+		add: function(exp1, exp2) {},
+		subtract: function(exp1, exp2) {},
+		power: function(exp1, exp2) {},
+		outerPower: function(exp1, exp2) {},
+		multiply: function(exp1, exp2) {},
+		innerProduct: function(exp1, exp2) {},
+		outerProduct: function(exp1, exp2) {},
+		div: function(exp1, exp2) {},
+		backdiv: function(exp1, exp2) {},
 
-	funcall: function(name, args) {}
-};
+		funcall: function(name, args) {}
+	};
+	
+	return yyobj;
+}();
 exports.yy = yy;
